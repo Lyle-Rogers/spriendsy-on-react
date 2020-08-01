37633032e9b3d42db800849d8050fd4a0219388f
Lyle-Rogers <lylerogers8@gmail.com>
2020-08-01 15:22:48 -0700

    after creating the librairy of wisdome

--- src/components/pages/archive.js
+++ src/components/pages/archive.js
@@ -3,13 +3,110 @@ import React from 'react';
 export default function() {
     return (
         <div className="archive-wrapper"> 
-            
-            <div className="book-librairy">
-                <div className="librairy-header">
-                    <h1>ARCHIVE</h1>
+            <div className="librairy-header">
+                <h1>LIBRAIRY OF WISDOME</h1>
+            </div>
+            <div className="book-librairy-wrapper">
+                <div className="books-container">
+                    <div className="books">
+                        <a className="flower-of-life-v1" href="https://www.pdfdrive.com/the-ancient-secret-of-the-flower-of-life-volume-1-ancient-secret-of-the-flower-of-life-e184934660.html" />
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-ancient-secret-of-the-flower-of-life-volume-1-ancient-secret-of-the-flower-of-life-e184934660.html">
+                            <h6>The Ancient Secret of the Flower of Life, Volume 1</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="flower-of-life-v2" href="https://www.pdfdrive.com/the-ancient-secret-of-the-flower-of-life-volume-2-ancient-secret-of-the-flower-of-life-e184253763.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-ancient-secret-of-the-flower-of-life-volume-2-ancient-secret-of-the-flower-of-life-e184253763.html">
+                            <h6>The Ancient Secret of the Flower of Life, Volume 2</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="cwg-1" href="https://www.pdfdrive.com/conversations-with-god-1-e60359914.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/conversations-with-god-1-e60359914.html">
+                            <h6>Conversations with God Book 1, An uncommon dialogue</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="cwg-2" href="https://www.pdfdrive.com/conversations-with-god-book-2-law-of-attraction-haven-e12731628.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/conversations-with-god-book-2-law-of-attraction-haven-e12731628.html">
+                            <h6>Conversations with God Book 2, An uncommon dialogue</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="cwg-3" href="https://www.pdfdrive.com/conversations-with-god-law-attraction-haven-e17722305.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/conversations-with-god-law-attraction-haven-e17722305.html">
+                            <h6>Conversations with God Book 3, An uncommon dialogue</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="cwg-4" href="https://www.pdfdrive.com/conversations-with-god-book-4-awaken-the-species-e186488002.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/conversations-with-god-book-4-awaken-the-species-e186488002.html">
+                            <h6>Conversations with God Book 4, Awaken the Species</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="sapiens" href="https://www.pdfdrive.com/sapiens-a-brief-history-of-humankind-e175870479.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/sapiens-a-brief-history-of-humankind-e175870479.html">
+                            <h6>Sapiens: A Brief History of Humankind</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="auto-biography-of-a-yogi" href="https://www.pdfdrive.com/autobiography-of-a-yogi-e184149575.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/autobiography-of-a-yogi-e184149575.html">
+                            <h6>Autobiography of a Yogi</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="the-subtle-art-of-not-giving-a-fuck" href="https://www.pdfdrive.com/the-subtle-art-of-not-giving-a-fuck-e200709293.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-subtle-art-of-not-giving-a-fuck-e200709293.html">
+                            <h6>The Subtle Art of Not Giving a Fuck</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="change-your-thoughts-change-your-life" href="https://www.pdfdrive.com/change-your-thoughts-change-your-life-living-the-wisdom-of-the-tao-e199753772.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/change-your-thoughts-change-your-life-living-the-wisdom-of-the-tao-e199753772.html">
+                            <h6>Change Your Thoughts: Change Your Life: Living the Wisdom of the Tao</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="the-awakening-psychic" href="https://www.pdfdrive.com/the-awakened-psychic-what-you-need-to-know-to-develop-your-psychic-abilities-e194659655.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-awakened-psychic-what-you-need-to-know-to-develop-your-psychic-abilities-e194659655.html">
+                            <h6>The awakened psychic: what you need to know to develop your psychic abilities</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="the-emerald-tablets" href="https://www.pdfdrive.com/the-emerald-tablets-of-thoth-the-atlantean-e161318431.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-emerald-tablets-of-thoth-the-atlantean-e161318431.html">
+                            <h6>The Emerald Tablets of Thoth The Atlantean</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="living-with-the-himalayan-masters" href="https://www.pdfdrive.com/living-with-the-himalayan-masters-e188524148.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/living-with-the-himalayan-masters-e188524148.html">
+                            <h6>Living with the Himalayan Masters</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="the-ra-contact" href="https://www.pdfdrive.com/the-ra-contact-teaching-the-law-of-one-volume-1-2-e195296487.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-ra-contact-teaching-the-law-of-one-volume-1-2-e195296487.html">
+                            <h6>The Ra Contact: Teaching the Law of One: Volume 1 & 2</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="the-power-of-now" href="https://www.pdfdrive.com/the-power-of-now-a-guide-to-spiritual-enlightenment-e176376600.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-power-of-now-a-guide-to-spiritual-enlightenment-e176376600.html">
+                            <h6>The Power of Now: A Guide to Spiritual Enlightenment</h6>
+                        </a>
+                    </div>
+                    <div className="books">
+                        <a className="the-prophet" href="https://www.pdfdrive.com/the-prophet-e162489909.html"/>
+                        <a className="book-titles" href="https://www.pdfdrive.com/the-prophet-e162489909.html">
+                            <h6>The Prophet</h6>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className="the-saying-wrapper">
+            <div className="memoriam-wrapper">
                 <h1 className="the-saying-header">
                     MEMORIAM
                 </h1>
